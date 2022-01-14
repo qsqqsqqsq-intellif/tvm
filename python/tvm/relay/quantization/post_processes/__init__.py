1102dddcb6782dfa52d63b1f10ea8c207e0ef30f
yang.haihui@intellif.com <yang.haihui@intellif.com>
2022-01-13 19:45:17 -0800

    post_process init for nnp300

--- python/tvm/relay/quantization/post_processes/__init__.py
+++ python/tvm/relay/quantization/post_processes/__init__.py
@@ -14,10 +14,13 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-# pylint: disable=unused-argument,inconsistent-return-statements
+# pylint: disable=unused-argument,inconsistent-return-statements,bare-except
 """Automatic quantization utilities."""
 
-from .eliminate_quantize_dequantize import eliminate_quantize_dequantize
-from .eliminate_dequantize_quantize import eliminate_dequantize_quantize
+try:
+    from .eliminate_quantize_dequantize import eliminate_quantize_dequantize
+    from .eliminate_dequantize_quantize import eliminate_dequantize_quantize
+except:
+    pass
 from .remove_input_quantize import remove_input_quantize
 from .extract_module import extract_module
